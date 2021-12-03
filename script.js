3a671d4fb51bfbd18dd588f1f504f3d2278d3c99
Bálint Táborszki <46889694+merewif@users.noreply.github.com>
2021-12-03 12:03:37 +0100

    jQuery slideToggle

--- script.js
+++ script.js
@@ -8,3 +8,35 @@ function hide() {
 window.addEventListener ("load", function() {
     setTimeout(hide, 3000);
 })
+
+$(document).ready(function(){
+    $('#upper1').click(function() {
+        $('.explanation').not('#explanation1').slideUp('slow');
+        $('#explanation1').slideToggle('slow');
+    });
+
+    $('#upper2').click(function() {
+        $('.explanation').not('#explanation2').slideUp('slow');
+        $('#explanation2').slideToggle('slow');
+    });
+
+    $('#upper3').click(function() {
+        $('.explanation').not('#explanation3').slideUp('slow');
+        $('#explanation3').slideToggle('slow');
+    });
+
+    $('#upper4').click(function() {
+        $('.explanation').not('#explanation4').slideUp('slow');
+        $('#explanation4').slideToggle('slow');
+    });
+
+    $('#upper5').click(function() {
+        $('.explanation').not('#explanation5').slideUp('slow');
+        $('#explanation5').slideToggle('slow');
+    });
+
+    $('#upper6').click(function() {
+        $('.explanation').not('#explanation6').slideUp('slow');
+        $('#explanation6').slideToggle('slow');
+    });
+});
